8c7a94e2c7d8dc03a5b538db654a331c8d70be73
Daniel Campbell <danielcampbell2097@hotmail.com>
2020-02-11 14:46:30 +0000

    minor changes to API Description

--- cross-nlp-rest-api/src/main/java/org/crossnlp/api/ApiDescription.java
+++ cross-nlp-rest-api/src/main/java/org/crossnlp/api/ApiDescription.java
@@ -3,10 +3,10 @@ package org.crossnlp.api;
 public class ApiDescription {
 	
 	public static final String title = "CROSS NLP REST API";
-	public static final String description = "A REST API featuring acollection of natural language processing tools for the software engineering domain";
+	public static final String description = "A REST API featuring a collection of natural language processing tools for the software engineering domain";
 	public static final String version  = "1.0.0";
-	public static final String termsOfServiceUrl = "https://url.to.TOS.com";
-	public static final String contactName= "Danny2097";
+	//public static final String termsOfServiceUrl = "https://url.to.TOS.com";
+	public static final String contactName= "Dan Campbell";
 	public static final String contactUrl = "https://github.com/Danny2097";
 	public static final String contactEmail = "danielcampbell2097@hotmail.com";
 	public static final String license = "Apache License 2.0";
